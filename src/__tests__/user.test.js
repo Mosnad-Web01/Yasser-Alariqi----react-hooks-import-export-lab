a7e0eb586468aae6fb6ede117afd028b24ccf2d2
YasserEsam <yaaser10esam1010@gmail.com>
2024-09-01 14:14:44 +0300

    pass the test and change the image link bbecause it is not working

--- src/__tests__/user.test.js
+++ src/__tests__/user.test.js
@@ -3,5 +3,5 @@ import { username, city, image } from "../data/user";
 test("username, city, and image are all exported", () => {
   expect(username).toEqual("Liza");
   expect(city).toEqual("New York");
-  expect(image).toEqual("https://i.imgur.com/mV8PQxj.gif");
+  expect(image).toEqual("https://adobe.scene7.com/is/image/AdobeCreate/DSC_ArtOfTrace_HT_0001_Trace_flat_round?$small$");
 });
--- src/data/user.js
+++ src/data/user.js
@@ -1,6 +1,8 @@
 const username = "Liza";
 const city = "New York";
 
-const image = "https://i.imgur.com/mV8PQxj.gif";
+const image = "https://adobe.scene7.com/is/image/AdobeCreate/DSC_ArtOfTrace_HT_0001_Trace_flat_round?$small$";
 
 // export all three variables as named exports
+
+export {username , city , image}
